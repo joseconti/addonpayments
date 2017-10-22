cb308a0a6e7b75711f21e83d24dc2f9eb4c149e5
José Conti <j.conti@joseconti.com>
2017-10-22 19:13:07 +0200

    Added AddonPayments to WordPress admin menu

--- core/menu/loader-menu.php
+++ core/menu/loader-menu.php
@@ -3,15 +3,15 @@
 
 	require_once( ADDONP_PLUGIN_PATH . 'core/pages/settings.php' );
 
-	add_action('admin_menu', 'addon_register_settings_submenu_page');
+	add_action('admin_menu', 'addonp_register_settings_submenu_page');
 
-	function addon_register_settings_submenu_page() {
+	function addonp_register_settings_submenu_page() {
 	   $addon_settings = add_submenu_page(
 					        'options-general.php',
 					        'AddonPayments',
 					        'AddonPayments',
 					        'manage_options',
 					        'addonpayments-settings',
-					        'addon_register_settings_submenu_page_callback'
+					        'addonp_register_settings_submenu_page_callback'
 					      );
 	}
