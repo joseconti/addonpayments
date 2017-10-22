c56e405daba3eaba41608ad83bfa6ac98174ebc7
José Conti <j.conti@joseconti.com>
2017-10-23 01:07:05 +0200

    functions file loaded

--- core/loader-core.php
+++ core/loader-core.php
@@ -2,4 +2,5 @@
 	if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 
+	require_once( ADDONP_PLUGIN_PATH . 'core/functions/functions.php' );
 	require_once( ADDONP_PLUGIN_PATH . 'core/menu/loader-menu.php' );
